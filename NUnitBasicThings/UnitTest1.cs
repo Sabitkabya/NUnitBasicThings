c4011a3cef727c71d966d709ddfd325ed8e454d0
gyanendra basnet <bgyanendra93@gmail.com>
2020-12-31 16:13:14 -0600

    initial commit

---
+++ NUnitBasicThings/UnitTest1.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Tests
+{
+    public class Tests
+    {
+        IWebDriver driver;
+        [SetUp]                             
+        public void Setup()
+        {
+            driver = new ChromeDriver(@"D:\Drivers\chromedriver_win32 _86");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
+        }
+        [Test]
+        public void CheckBoxTest()
+        {
+            driver.Url = "https://www.fdmdigital.co.uk/formidable-forms-radio-buttons-checkbox-styling/";
+            IReadOnlyCollection<IWebElement>checkBoxes = driver.FindElements(By.Id("frm_checkbox_944-933"));
+            foreach (IWebElement checkBox in checkBoxes)
+            {
+                if (!checkBox.Enabled)
+                {
+                    checkBox.Click();
+                    Thread.Sleep(5000);
+                }
+
+                else
+                {
+
+                }
+                
+            }
+        }
+    }
+}
